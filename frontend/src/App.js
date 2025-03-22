ac1556686d64886a3482e694ffc03f46dbaa1ba6
Dylan Rylee A. Dizon <109629195+dylanrylee@users.noreply.github.com>
2025-03-22 15:30:36 -0600

    PaymentCheckout for Debit done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SelectAccountType from './guestUI/SelectAccountType';
 import GuestLogin from './guestUI/GuestLogin';
 import GuestHomePage from './guestUI/GuestHomePage';
+import PaymentCheckout from './guestUI/PaymentCheckout';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<SelectAccountType />} />
+                {/* <Route path="/" element={<SelectAccountType />} />
                 <Route path="/guest-login" element={<GuestLogin />} />
-                <Route path="/guest-home" element={<GuestHomePage />} />
+                <Route path="/guest-home" element={<GuestHomePage />} /> */}
+                <Route path="/" element={<PaymentCheckout />}></Route>
             </Routes>
         </Router>
     );
