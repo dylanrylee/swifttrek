9463cca24bb8b094fddb252962e2cb158a72f117
zawadsafwan07 <109509884+zawadsafwan07@users.noreply.github.com>
2025-04-14 21:58:35 -0600

    Delete frontend/src/adminUI directory

--- frontend/src/adminUI/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from './AuthContext';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PrivateRoute = () => {
-  const { currentUser } = useAuth();
-  return currentUser ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
