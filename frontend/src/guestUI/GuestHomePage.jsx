7ed35942a3ad468bd9ba258287e14dbf35a567df
Dylan Rylee Dizon <dylanrylee10@gmail.com>
2025-04-15 00:24:14 -0600

    Update GuestHomePage.jsx

--- frontend/src/guestUI/GuestHomePage.jsx
+++ frontend/src/guestUI/GuestHomePage.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import styles from './GuestHomePage.module.css';
 import Header from './Header';
 import Footer from './Footer';
-import frontpageImage from '../images/guest-frontpage.png';
 
 const GuestHomePage = () => {
     const navigate = useNavigate();
@@ -12,12 +11,10 @@ const GuestHomePage = () => {
         <div className={styles.container}>
             <Header />
             <main className={styles.mainContent}>
-                <img src={frontpageImage} alt="Welcome" className={styles.welcomePhoto} />
                 <div className={styles.buttonContainer}>
                     <button 
                         className={styles.actionButton}
                         onClick={() => navigate('/plane-booking')}
-                    
                     >
                         Book Flight
                     </button>
